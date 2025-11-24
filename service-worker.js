7b244e0b65c7c611fef95f46c2e01cec8980149f
GDoraA <galbacsdori@gmail.com>
2025-11-24 01:21:02 +0100

    d

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("gda-cache").then(cache => {
+      return cache.addAll([
+        "/index.html",
+        "/styles.css",
+        "/app.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
